31f823a32d3d604c0d96043b773acd43d5f5d0c7
Umayarz18 <61628982+Umayarz18@users.noreply.github.com>
2021-11-15 02:29:26 -0800

    Added a fallback route

--- web/next.config.js
+++ web/next.config.js
@@ -5,5 +5,9 @@ module.exports = withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    fallbacks: {
+      document: '/pages/fallback.js',
+    },
   },
 })
---
+++ web/pages/fallback.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Fallback() {
+    return (
+
+        <div>
+            <h1>This is fallback page when device is offline </h1>
+            <small>Route will fallback to this page</small>
+        </div>
+    )
+}
